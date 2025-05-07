bb82764e7fc08c4dc139ceef3339401ae1268443
unknown <melkywafula29@gmail.com>
2025-05-07 16:48:23 +0300

    more updates

--- Backend/controllers/appointmentController.js
+++ Backend/controllers/appointmentController.js
@@ -3,7 +3,7 @@ const Appointment = require('../models/Appointment');
 exports.createAppointment = async (req, res) => {
   try {
     const appointment = await Appointment.create(req.body);
-    res.json(appointment);
+    res.status(201).json(appointment); // Use 201 status code for created resource
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -13,7 +13,7 @@ exports.getAppointments = async (req, res) => {
   try {
     // Fetch appointments and populate patient and doctor details
     const appointments = await Appointment.find()
-    .populate('patientId', '_id name')
+      .populate('patientId', '_id name')
       .populate('doctorId', 'name'); // Populate only the 'name' field from User (doctor)
 
     res.json(appointments);
@@ -27,7 +27,11 @@ exports.getAppointmentsByDoctorId = async (req, res) => {
     const { doctorId } = req.params;
     const appointments = await Appointment.find({ doctorId })
       .populate('patientId', '_id name')
-      .populate('doctorId', 'name');
+      .populate('doctorId', 'name'); // Populate doctor details only by name
+
+    if (appointments.length === 0) {
+      return res.status(404).json({ message: 'No appointments found for this doctor' });
+    }
 
     res.json(appointments);
   } catch (err) {
